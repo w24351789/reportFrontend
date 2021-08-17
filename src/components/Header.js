65092e10ee75ab1ef51629b58a181fb88a53fa56
TED <W24351789@GMAIL.COM>
2021-08-17 14:52:19 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                Hello From Component
+            </h1>
+        </header>
+    )
+}
+
+export default Header
